32864a9bd4d6afa52719c8f7bb17dcc512f4a564
segunNew <eeomo007@gmail.com>
2024-12-23 02:23:34 +0100

    :lipstick: new fix on revamp UI 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -133,12 +133,17 @@ const Navbar = () => {
 
           <div style={{background: "linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 92, 186, 1))",
           margin: "0 auto"
-  }}  className={`space-y-4 pt-0 absolute left-0 lg:left-[4rem] rounded-3xl pb-5 w-[100%] lg:w-[90%] flex flex-col text-center justify-evenly md:flex-row md:text-left lg:flex-row gap-3 z-10 h-[90vh] lg:h-[400px] overflow-hidden
+  }}  className={`space-y-4 pt-0 absolute left-0 lg:left-[4rem] rounded-3xl pb-14 md:pb-5 w-[100%] lg:w-[90%] flex flex-col text-center justify-evenly md:flex-row md:text-left lg:flex-row gap-3 z-10 h-[90vh] lg:h-[400px] overflow-hidden
     ${isMenuOpened ? "opacity-100 translate-y-0 visible pointer-events-auto" : "opacity-0 -translate-y-5 invisible pointer-events-none"} 
     transition-all duration-500 ease-in-out`}
 >
          {navItems.map(({ link, path, icon, submenu }, index) => (
-    <div key={link} className="px-0 lg:px-14 mt-4 space-y-2 mx-auto text-center md:text-left">
+     <div
+     key={link}
+     className={`px-0 lg:px-14 mt-4 space-y-2 mx-auto text-center md:text-left ${
+       index === 0 ? "justify-start" : ""
+     }`}
+   >
       <div className="flex hover:text-gray-200 text-white items-center hover:text-secondary transition-all duration-300">
         <a href={path} className="block py-1 text-2xl font-bold">
           {link}
@@ -156,14 +161,20 @@ const Navbar = () => {
       </div>
       <hr className="hidden md:block" style={{ margin: "0" }} />
       {/* Render Submenu */}
-      <div className={`transition-all duration-300 ease-in ${openSubmenuIndex === index ? "block" : "hidden"} md:block`}>
+      <div
+  className={`overflow-hidden transition-[max-height, opacity] duration-500 ease-in-out ${
+    openSubmenuIndex === index
+      ? "opacity-100 visible max-h-96"
+      : "opacity-0 invisible max-h-0"
+  } md:visible md:opacity-100 md:max-h-96`}
+>      
         {submenu && (
-          <div className="space-y-2 transition-all duration-300 ease-in">
+          <div className="space-y-2 transition-all duration-500 ease-in">
             {submenu.map(({ link, path }) => (
               <a
                 key={link}
                 href={path}
-                className="block py-2 hover:rounded-md transition-all duration-300 text-white hover:text-secondary"
+                className="block py-2 hover:rounded-md transition-all duration-500 text-white hover:text-secondary"
               >
                 {link}
               </a>
@@ -174,8 +185,7 @@ const Navbar = () => {
 
     </div>
   ))}
-  <a href='login' className='bg-secondary py-3 mx-[2rem] rounded-full text-primary hover:text-white hover:bg-primary text-sm font-semibold md:hidden'>Login</a>
-
+  <a href='login' className='bg-secondary py-3 mx-[5rem] rounded-full text-primary hover:text-white hover:bg-primary text-sm font-semibold md:hidden'>Login</a>
 </div>
 
         </nav> 
